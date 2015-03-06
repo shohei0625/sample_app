c273cdb7a9c62a368d8860666932f44ae5a74e1c
shohei0625 <nsmkaa@gmail.com>
2015-03-06 05:35:00 +0000

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :microposts, dependent: :destroy
      before_save { self.email = email.downcase }
 validates :name, presence: true, length: { maximum: 50 }
 VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -6,12 +7,17 @@ validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
 uniqueness: { case_sensitive: false }
 has_secure_password
 validates :password, length: { minimum: 6 }
-  def User.new_remember_token
+   def User.new_remember_token
     SecureRandom.urlsafe_base64
    end
 
   def User.encrypt(token)
     Digest::SHA1.hexdigest(token.to_s)
+  end
+  def feed
+    # このコードは準備段階です。
+    # 完全な実装は、次章を参照してください。
+    Micropost.where("user_id = ?", id)
   end
 
   private
